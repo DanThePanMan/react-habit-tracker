8c71b238b5ce58e3c55e0193343b23079d44eab3
Daniel <danielchenschw@gmail.com>
2023-12-26 00:20:30 -0500

    Fix Routing Issue

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ import classes from "./App.module.css";
 function App() {
   return (
     <div className={classes.Content}>
-      <Router>
+      <Router basename="/react-habit-tracker">
         <SideBar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="/new" element={<NewHabit />} />
-          <Route path="/search" element={<Search />} />
+          <Route path="/react-habit-tracker" element={<Home />} />
+          <Route path="/react-habit-tracker/schedule" element={<Schedule />} />
+          <Route path="/react-habit-tracker/new" element={<NewHabit />} />
+          <Route path="/react-habit-tracker/search" element={<Search />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
