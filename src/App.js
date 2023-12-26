cf4be0d7e744dbf871084cb891d02755cbc470c5
Daniel <danielchenschw@gmail.com>
2023-12-26 00:12:06 -0500

    Switch to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import SideBar from "./Components/SideBar";
 import Home from "./Pages/Home";
@@ -13,7 +13,7 @@ import classes from "./App.module.css";
 function App() {
   return (
     <div className={classes.Content}>
-      <Router basename="react-habit-tracker">
+      <Router>
         <SideBar />
         <Routes>
           <Route path="/" element={<Home />} />
