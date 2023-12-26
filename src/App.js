231d4d0b4d99d56e576aaf894cd5b39042b80fa7
Daniel <danielchenschw@gmail.com>
2023-12-26 00:14:18 -0500

    Return To BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SideBar from "./Components/SideBar";
 import Home from "./Pages/Home";
