b5ff6a304dbb8ea54cbb2cd80b309bea1eaddfd2
Daniel <danielchenschw@gmail.com>
2024-01-02 22:07:30 -0500

    Complete Structure for Home Page

---
+++ src/Components/GraphOfTasks/GraphOfTasks.jsx
@@ -0,0 +1,138 @@
+import classes from "./GraphOfTasks.module.css";
+
+function GraphOfTasks(props) {
+  const data = props.data;
+
+  const Mondays = [];
+  const Tuesdays = [];
+  const Wednesdays = [];
+  const Thursdays = [];
+  const Fridays = [];
+  const Saturdays = [];
+  const Sundays = [];
+
+  for (const key in data) {
+    data[key].FirebaseKey = key;
+    if (data[key].status === true) {
+      switch (data[key].day) {
+        case "mon":
+          Mondays.push(data[key]);
+          break;
+        case "tue":
+          Tuesdays.push(data[key]);
+          break;
+        case "wed":
+          Wednesdays.push(data[key]);
+          break;
+        case "thu":
+          Thursdays.push(data[key]);
+          break;
+        case "fri":
+          Fridays.push(data[key]);
+          break;
+        case "sat":
+          Saturdays.push(data[key]);
+          break;
+        case "sun":
+          Sundays.push(data[key]);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  return (
+    <>
+      <div className={classes.DataBars}>
+        <div className={classes.Bar}>
+          {Mondays.map((habit) => {
+            const BarColor = habit.color;
+            const barHeight = habit.time[0];
+            const style = {
+              backgroundColor: BarColor,
+              height: barHeight * 10 + "px",
+              width: "64px",
+            };
+            return <div key={habit.FirebaseKey} style={style}></div>;
+          })}
+        </div>
+        <div className={classes.Bar}>
+          {Tuesdays.map((habit) => {
+            const BarColor = habit.color;
+            const barHeight = habit.time[0];
+            const style = {
+              backgroundColor: BarColor,
+              height: barHeight * 10 + "px",
+              width: "64px",
+            };
+            return <div key={habit.FirebaseKey} style={style}></div>;
+          })}
+        </div>
+        <div className={classes.Bar}>
+          {Wednesdays.map((habit) => {
+            const BarColor = habit.color;
+            const barHeight = habit.time[0];
+            const style = {
+              backgroundColor: BarColor,
+              height: barHeight * 10 + "px",
+              width: "64px",
+            };
+            return <div key={habit.FirebaseKey} style={style}></div>;
+          })}
+        </div>
+        <div className={classes.Bar}>
+          {Thursdays.map((habit) => {
+            const BarColor = habit.color;
+            const barHeight = habit.time[0];
+            const style = {
+              backgroundColor: BarColor,
+              height: barHeight * 10 + "px",
+              width: "64px",
+            };
+            return <div key={habit.FirebaseKey} style={style}></div>;
+          })}
+        </div>
+        <div className={classes.Bar}>
+          {Fridays.map((habit) => {
+            const BarColor = habit.color;
+            const barHeight = habit.time[0];
+            const style = {
+              backgroundColor: BarColor,
+              height: barHeight * 10 + "px",
+              width: "64px",
+            };
+            return <div key={habit.FirebaseKey} style={style}></div>;
+          })}
+        </div>
+        <div className={classes.Bar}>
+          {Saturdays.map((habit) => {
+            const BarColor = habit.color;
+            const barHeight = habit.time[0];
+            const style = {
+              backgroundColor: BarColor,
+              height: barHeight * 10 + "px",
+              width: "64px",
+            };
+            return <div key={habit.FirebaseKey} style={style}></div>;
+          })}
+        </div>
+        <div className={classes.Bar}>
+          {Sundays.map((habit) => {
+            const BarColor = habit.color;
+            const barHeight = habit.time[0];
+            const style = {
+              backgroundColor: BarColor,
+              height: barHeight * 10 + "px",
+              width: "64px",
+            };
+            return <div key={habit.FirebaseKey} style={style}></div>;
+          })}
+        </div>
+      </div>
+      <div className={classes.Dine}></div>
+    </>
+  );
+}
+
+export default GraphOfTasks;
--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,27 +1,84 @@
 import Card from "../../Components/card/Card";
-import classes from "./home.module.css"
-import { motion } from "framer-motion"
+import classes from "./home.module.css";
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+import GraphOfTasks from "../../Components/GraphOfTasks/GraphOfTasks";
 
-function Home(){
-    return (
-        <motion.div 
-        className={classes.PageContent}
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        exit={{opacity: 0}}
-        transition={{duration: 0.13}}
-        >
-            <div className={classes.LeftCluster}>
-                <Card>
-                    <h1>Hi, Here is a quick view of your week</h1>
-                </Card>
-                <Card>
-                    <h2>woo</h2>
-                </Card>
-            </div>
-            <Card>goo</Card>
-        </motion.div>
+function Home() {
+  const [userData, setUserData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-habit-tracker-2c7cd-default-rtdb.firebaseio.com/habitData.json"
     )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const habits = [];
+
+        for (const key in data) {
+          const habit = {
+            id: key,
+            ...data[key],
+          };
+          habits.push(habit);
+        }
+        setIsLoading(false);
+        setUserData(data);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div className={classes.Loading}>loading...</div>;
+  }
+
+  const ListOfCompletes = [];
+
+  for (const key in userData) {
+    userData[key].FirebaseKey = key;
+    if (userData[key].status === true) {
+      ListOfCompletes.push(userData[key]);
+    }
+  }
+
+  return (
+    <motion.div
+      className={classes.PageContent}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.13 }}
+    >
+      <div className={classes.LeftCluster}>
+        <Card>
+          <h1>Hi, Here is a quick view of your week</h1>
+        </Card>
+        <Card>
+          <div className={classes.Graph}>
+            <GraphOfTasks data={userData} />
+          </div>
+        </Card>
+      </div>
+      <div className={classes.FinishedTasks}>
+        <div className={classes.FinishedTasksTitle}></div>
+        {ListOfCompletes.map((habit) => {
+          return (
+            <div key={habit.FirebaseKey} className={classes.HabitCard}>
+              <Card>
+                <div className={classes.CardContent}>
+                  <div className={classes.HabitTitle}>{habit.title}</div>
+                  <div>{habit.description}</div>
+                  <div>{`${habit.time[0]} Hour(s) and ${habit.time[1]} Minute(s)`}</div>
+                </div>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+    </motion.div>
+  );
 }
 
 export default Home;
